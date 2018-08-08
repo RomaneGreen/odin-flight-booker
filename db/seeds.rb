afed2cb0947133d333e6bd47b7deecfcadbc8b3c
Romane <romane71193@gmail.com>
2018-08-08 12:06:31 -0400

    add form flight dates

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,7 @@ Airport.create(code: 'POP-PuertaPlata,DR')
 Airport.create(code: 'MDE-Medellin,Columbia')
 Airport.create(code: 'GIG-Rio de janiero')
 Airport.create(code: 'CRK-Angeles City,Phillipines')
+
+Flight.create(airport_id:1,flight_date:10.days.from_now,flight_duration:"2 hours")
+Flight.create(airport_id:2,flight_date:20.days.from_now,flight_duration:"3 hours")
+Flight.create(airport_id:3,flight_date:30.days.from_now,flight_duration:"4 hours")
