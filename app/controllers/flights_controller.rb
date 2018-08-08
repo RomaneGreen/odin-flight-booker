47094623d56acd6fe525b5c1ec97f80e778134b2
Romane <romane71193@gmail.com>
2018-08-08 13:53:56 -0400

    add search query results

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,7 +4,10 @@ class FlightsController < ApplicationController
 
 @flight = Flight.new
 
-
+@airport_id = params[:flight]
+@passengers = params[:passengers]
+@flight_date = params[:flight_dates]
+@flight_id = params[:passengers].to_i + rand(10)
   end
 
 def create
